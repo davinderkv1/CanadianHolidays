7ddbdec72ace3667da893c59580a31a6254f3830
Davinderkv1 <Davinderkv1@rediffmail.com>
2023-10-31 17:05:49 -0700

    Add project files.

---
+++ CanadianHolidays/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CanadianHolidays
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CanadianHolidays/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CanadianHolidays
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
