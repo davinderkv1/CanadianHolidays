7e6785fe80fb20e23a7e880ba7642c85a6010782
Davinderkv1 <Davinderkv1@rediffmail.com>
2023-10-31 17:39:06 -0700

    Added some more Holidays

--- CanadianHolidays/viewModels/HolidaysViewModel.cs
+++ CanadianHolidays/viewModels/HolidaysViewModel.cs
@@ -11,11 +11,23 @@ namespace CanadianHolidays.ViewModels
         {
             Holidays = new ObservableCollection<Holiday>
             {
-                new Holiday { Name = "New Year's Day", Date = new DateTime(DateTime.Now.Year, 1, 1) },
-                new Holiday { Name = "Good Friday", Date = new DateTime(DateTime.Now.Year, 4, 15) },
-                new Holiday { Name = "Victoria Day", Date = new DateTime(DateTime.Now.Year, 5, 24) },
-                new Holiday { Name = "Canada Day", Date = new DateTime(DateTime.Now.Year, 7, 1) },
-                new Holiday { Name = "Civic Day", Date = new DateTime(DateTime.Now.Year, 8, 2) }
+                new Holiday { Name = "Family Day", Date = new DateTime(2023, 2, 20) }, // Observed in several provinces
+new Holiday { Name = "Islander Day", Date = new DateTime(2023, 2, 20) }, // Prince Edward Island
+new Holiday { Name = "Louis Riel Day", Date = new DateTime(2023, 2, 20) }, // Manitoba
+new Holiday { Name = "Heritage Day", Date = new DateTime(2023, 2, 20) }, // Nova Scotia
+new Holiday { Name = "Discovery Day", Date = new DateTime(2023, 2, 20) }, // Yukon
+new Holiday { Name = "St. Patrick's Day", Date = new DateTime(2023, 3, 17) }, // Newfoundland and Labrador (not statutory)
+new Holiday { Name = "St. George's Day", Date = new DateTime(2023, 4, 23) }, // Newfoundland and Labrador (not statutory)
+new Holiday { Name = "National Patriots' Day", Date = new DateTime(2023, 5, 22) }, // Quebec
+new Holiday { Name = "National Indigenous Peoples Day", Date = new DateTime(2023, 6, 21) }, // Not a statutory holiday
+new Holiday { Name = "Discovery Day", Date = new DateTime(2023, 6, 24) }, // Newfoundland and Labrador
+new Holiday { Name = "Orangemen's Day", Date = new DateTime(2023, 7, 12) }, // Newfoundland and Labrador (not statutory)
+new Holiday { Name = "Regatta Day", Date = new DateTime(2023, 8, 2) }, // St. John's, Newfoundland and Labrador
+new Holiday { Name = "Gold Cup Parade Day", Date = new DateTime(2023, 8, 18) }, // Prince Edward Island (not statutory)
+new Holiday { Name = "Discovery Day", Date = new DateTime(2023, 8, 21) }, // Yukon
+new Holiday { Name = "Labour Day", Date = new DateTime(2023, 9, 4) },
+new Holiday { Name = "Thanksgiving Day", Date = new DateTime(2023, 10, 9) }
+
             };
         }
     }
